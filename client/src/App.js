ceab4c52e9bc0868be40360882c7396ea1050b8a
Apoorv Garg <u.garg.10@gmail.com>
2020-09-13 13:50:43 +0530

    Commit Client

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import NavBar from './components/Navbar';
+import "./App.css"
+import {BrowserRouter, Route} from 'react-router-dom'
+
+function App() {
+  return (< BrowserRouter > <NavBar / > <Route path = "/" > < /Route> < /BrowserRouter >);
+}
+
+export default App;
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (<div class="nav-wrapper white">
+    <a href="#" className="brand-logo left">
+      Logo < /a>
+      <ul id="nav-mobile" className="right">
+        <li >
+          < a href="sass.html">
+            Login < /a></li >
+          <li >
+            < a href="badges.html">
+              Signup < /a></li >
+            <li >
+              < a href="collapsible.html">
+                Profile < /a></li >
+            </ul>
+          </div >
+          )
+}
+
+export default NavBar
